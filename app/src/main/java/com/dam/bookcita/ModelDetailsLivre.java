e75c94fb05f6f553e3720044558b1fa650c01f4b
BookCita <bookcita9@gmail.com>
2022-06-24 13:39:07 +0200

    Améliorer le design pour les deux interfaces

--- app/src/main/java/com/dam/bookcita/ModelDetailsLivre.java
+++ app/src/main/java/com/dam/bookcita/ModelDetailsLivre.java
@@ -7,17 +7,22 @@ public class ModelDetailsLivre {
     private String date_parution_livre;
     private String resume_livre;
     private String url_cover_livre;
+    private String isbn_livre;
+    private int nombre_livres;
+    
 
     public ModelDetailsLivre() {
     }
 
-    public ModelDetailsLivre(String title_livre, String auteur_livre, String editeur_livre, String date_parution_livre, String resume_livre, String url_cover_livre) {
+    public ModelDetailsLivre(String title_livre, String auteur_livre, String editeur_livre, String date_parution_livre, String resume_livre, String url_cover_livre, String isbn_livre, int nombre_livres) {
         this.title_livre = title_livre;
         this.auteur_livre = auteur_livre;
         this.editeur_livre = editeur_livre;
         this.date_parution_livre = date_parution_livre;
         this.resume_livre = resume_livre;
         this.url_cover_livre = url_cover_livre;
+        this.isbn_livre = isbn_livre;
+        this.nombre_livres = nombre_livres;
     }
 
     public String getTitle_livre() {
@@ -66,5 +71,21 @@ public class ModelDetailsLivre {
 
     public void setUrl_cover_livre(String url_cover_livre) {
         this.url_cover_livre = url_cover_livre;
+    }
+
+    public String getIsbn_livre() {
+        return isbn_livre;
+    }
+
+    public void setIsbn_livre(String isbn_livre) {
+        this.isbn_livre = isbn_livre;
+    }
+
+    public int getNombre_livres() {
+        return nombre_livres;
+    }
+
+    public void setNombre_livres(int nombre_livres) {
+        this.nombre_livres = nombre_livres;
     }
 }
