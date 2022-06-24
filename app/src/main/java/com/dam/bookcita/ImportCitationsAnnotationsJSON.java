1474f04a0faa0ff63f2eef86fda3bcd6484f4511
BookCita <bookcita9@gmail.com>
2022-06-24 15:58:22 +0200

    Commencer la tache import citations et annotations

---
+++ app/src/main/java/com/dam/bookcita/ImportCitationsAnnotationsJSON.java
@@ -0,0 +1,46 @@
+package com.dam.bookcita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class ImportCitationsAnnotationsJSON extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_citations_annotations_json);
+    }
+
+    int requestCode = 1 ;
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode, data);
+        Context context = getApplicationContext();
+        if(requestCode == requestCode && resultCode == Activity.RESULT_OK){
+            if(data == null){
+                return;
+            }
+            Uri uri = data.getData();
+            Toast.makeText(context, uri.getPath(), Toast.LENGTH_SHORT).show();
+           // String path
+           // Log.i("tag", "URI  : ",  );
+
+        }
+    }
+
+    public void openFileChooser(View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(intent, requestCode);
+    }
+}
