b786a5ba64c7e4b567ffb1aebfc4fa0bf803d2df
entwan <entwan@gmail.com>
2022-06-29 12:18:02 +0200

    Menu bottom avec fragments (pour l'instant vides)

--- app/src/main/java/com/dam/bookcita/MainActivity.java
+++ app/src/main/java/com/dam/bookcita/MainActivity.java
@@ -1,14 +1,62 @@
 package com.dam.bookcita;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private FragmentAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager2);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Accueil"));
+        tabLayout.addTab(tabLayout.newTab().setText("Mes Livres"));
+        tabLayout.addTab(tabLayout.newTab().setText("Mes Citations"));
+        tabLayout.addTab(tabLayout.newTab().setText("Chercher"));
+        
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
+
+
 }
