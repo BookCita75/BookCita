f9f8fa46b6a430727a7e6ccda19b79bf5a95992d
entwan <entwan@gmail.com>
2022-06-30 17:01:25 +0200

    Ajout des 3 boutons pour ajouter citation

---
+++ app/src/main/java/com/dam/bookcita/AjoutCitationActivity.java
@@ -0,0 +1,14 @@
+package com.dam.bookcita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjoutCitationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_citation);
+    }
+}
