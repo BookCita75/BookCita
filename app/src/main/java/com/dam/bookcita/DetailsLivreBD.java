e5d3cf9bc4154b91df7b5f8e62bf8c9d31c4827e
BookCita <bookcita9@gmail.com>
2022-06-30 16:28:54 +0200

    Afficher un livre à partir de la base de donnée

---
+++ app/src/main/java/com/dam/bookcita/DetailsLivreBD.java
@@ -0,0 +1,14 @@
+package com.dam.bookcita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsLivreBD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_livre_bd);
+    }
+}
