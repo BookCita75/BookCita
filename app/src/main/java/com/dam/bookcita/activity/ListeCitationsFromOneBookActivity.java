cff49dd49c12be51e826dd3db4f5be63989fe9ee
entwan <entwan@gmail.com>
2022-07-05 13:51:20 +0200

    debut ListeCitationsFromOneBookActivity

---
+++ app/src/main/java/com/dam/bookcita/activity/ListeCitationsFromOneBookActivity.java
@@ -0,0 +1,16 @@
+package com.dam.bookcita.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dam.bookcita.R;
+
+public class ListeCitationsFromOneBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_citations_from_one_book);
+    }
+}
