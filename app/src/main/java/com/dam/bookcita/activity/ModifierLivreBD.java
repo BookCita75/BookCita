3aa8fd3a5ffec2134d1daeb0086d57097f0ef045
entwan <entwan@gmail.com>
2022-07-26 22:46:52 +0200

    pb image ModifierLivreBD corrigé

--- app/src/main/java/com/dam/bookcita/activity/ModifierLivreBD.java
+++ app/src/main/java/com/dam/bookcita/activity/ModifierLivreBD.java
@@ -51,9 +51,9 @@ public class ModifierLivreBD extends AppCompatActivity {
     private TextView tv_resume_livre;
     private TextView tv_isbn_livre;
     private TextView tv_nombres_pages_livres;
-    private ImageView iv_couverture_livre;
+    private ImageView iv_couverture_livre_bd;
     private EditText etLangueMLBD;
-    private static final String TAG = "UpadateLivreBD";
+    private static final String TAG = "ModifierLivreBD";
     private RequestQueue requestQueue;
 
     private StorageReference fileStorage;
@@ -61,6 +61,8 @@ public class ModifierLivreBD extends AppCompatActivity {
     private Uri serverFileUri;
 
     private Uri localFileUri;
+
+    private String tokenStorage = "bd2fee53-f293-469c-8dcd-14ae4b5562ec";
 
     private FirebaseAuth auth;
 
@@ -73,7 +75,7 @@ public class ModifierLivreBD extends AppCompatActivity {
     String uriPhoto;
 
 
-    public void initUI(){
+    public void initUI() {
         tv_title_livre = findViewById(R.id.et_titre_texte);
         tv_auteur_livre = findViewById(R.id.et_auteur_Livres);
         tv_editeur_livre = findViewById(R.id.et_editeur_Livre);
@@ -81,7 +83,7 @@ public class ModifierLivreBD extends AppCompatActivity {
         tv_resume_livre = findViewById(R.id.tv_updat_resume_livre_bd);
         tv_isbn_livre = findViewById(R.id.et_isbnLivre);
         tv_nombres_pages_livres = findViewById(R.id.et_nombre_pages);
-        iv_couverture_livre = (ImageView) findViewById(R.id.iv_couverture_livre_bd);
+        iv_couverture_livre_bd = findViewById(R.id.iv_couverture_livre_bd);
         tv_resume_livre.setMovementMethod(new ScrollingMovementMethod());
         etLangueMLBD = findViewById(R.id.etLangueMLBD);
 
@@ -93,6 +95,7 @@ public class ModifierLivreBD extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -111,30 +114,30 @@ public class ModifierLivreBD extends AppCompatActivity {
             public void onClick(View v) {
 
                 try {
-                    Log.i(TAG, "onClick: "+tv_auteur_livre.getText().toString());
+                    Log.i(TAG, "onClick: " + tv_auteur_livre.getText().toString());
 
                     int nombredespages = Integer.parseInt(tv_nombres_pages_livres.getText().toString());
-                    Log.i(TAG, "nombredespages: "+nombredespages);
+                    Log.i(TAG, "nombredespages: " + nombredespages);
                     if (tv_title_livre.getText().toString().equals("")) {
                         Toast.makeText(ModifierLivreBD.this, "Veuillez saisir un titre.", Toast.LENGTH_LONG).show();
                         return;
                     }
                     String langue = etLangueMLBD.getText().toString();
-                    if (!(langue.equals("fr") || langue.equals("en") || langue.equals("de")) ) {
+                    if (!(langue.equals("fr") || langue.equals("en") || langue.equals("de"))) {
                         Toast.makeText(ModifierLivreBD.this, "Veuillez choisir une langue entre fr, en ou de.", Toast.LENGTH_LONG).show();
                         return;
                     }
 
                     livresRef.document(id_BD).update(
                             TITRE_LIVRE_BD, tv_title_livre.getText().toString(),
-                            AUTEUR_LIVRE_BD,tv_auteur_livre.getText().toString(),
-                            EDITEUR_LIVRE_BD,tv_editeur_livre.getText().toString(),
-                            DATE_PARUTION_LIVRE_BD,tv_parution_livre.getText().toString(),
-                            RESUME_LIVRE_BD,tv_resume_livre.getText().toString(),
-                            ISBN_LIVRE_BD,tv_isbn_livre.getText().toString(),
-                            NB_PAGES_LIVRE_BD,nombredespages,
-                            LANGUE_LIVRE_BD,langue,
-                            URL_COVER_LIVRE_BD,uriPhoto
+                            AUTEUR_LIVRE_BD, tv_auteur_livre.getText().toString(),
+                            EDITEUR_LIVRE_BD, tv_editeur_livre.getText().toString(),
+                            DATE_PARUTION_LIVRE_BD, tv_parution_livre.getText().toString(),
+                            RESUME_LIVRE_BD, tv_resume_livre.getText().toString(),
+                            ISBN_LIVRE_BD, tv_isbn_livre.getText().toString(),
+                            NB_PAGES_LIVRE_BD, nombredespages,
+                            LANGUE_LIVRE_BD, langue,
+                            URL_COVER_LIVRE_BD, uriPhoto
 
                     );
 
@@ -154,13 +157,8 @@ public class ModifierLivreBD extends AppCompatActivity {
         });
 
 
-
-
-
-
-
-
     }
+
     public void loadImage(View v) {
 
         Log.i(TAG, "updateImage: imdn");
@@ -169,41 +167,56 @@ public class ModifierLivreBD extends AppCompatActivity {
 
     }
 
-    public void uploadImage(){
+    public void uploadImage() {
 
-        long time= System.currentTimeMillis();
-        String imageName = time+".jpg";
-        Log.i(TAG, "uploadImage: imageName"+imageName);
+        long time = System.currentTimeMillis();
+        String imageName = time + ".jpg";
+        Log.i(TAG, "uploadImage: imageName" + imageName);
         final StorageReference fileRef = fileStorage.child("couvertures_livres/" + imageName);
-        uriPhoto = "https://firebasestorage.googleapis.com/v0/b/bookcita.appspot.com/o/couvertures_livres%2F"+imageName+"?alt=media&token=c865477c-af5d-4bb7-821a-73dc83215583";
-        Log.i(TAG, "uploadImage: Uri photo : "+uriPhoto);
+
+        String fileStorageStr = fileStorage.toString();
+
+        Log.i(TAG, "uploadImage: fileStorageStr : " + fileStorageStr);
+        // fileStorageStr => gs://bookcita...
+        String fileStorageWithoutGs = fileStorageStr.substring(5);
+        uriPhoto = "https://firebasestorage.googleapis.com/v0/b/" + fileStorageWithoutGs + "o/couvertures_livres%2F" + imageName + "?alt=media&token=" + tokenStorage;
+        Log.i(TAG, "uploadImage: Uri photo : " + uriPhoto);
 
 
-        fileRef.putFile(localFileUri)
-                .addOnCompleteListener(ModifierLivreBD.this, new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+        try {
 
-                        if (task.isSuccessful()) {
-                            fileRef.getDownloadUrl().addOnSuccessListener(ModifierLivreBD.this,
-                                    new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    serverFileUri = uri;
-                                    Log.i(TAG, "onSuccess: On success"+uri.getPath());
-                                    //serverFileUri = uri.getPath();
+            fileRef.putFile(localFileUri)
+                    .addOnCompleteListener(ModifierLivreBD.this, new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
 
-                                }
-                            });
+                            if (task.isSuccessful()) {
+                                fileRef.getDownloadUrl().addOnSuccessListener(ModifierLivreBD.this,
+                                        new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
+                                                serverFileUri = uri;
+                                                Log.i(TAG, "onSuccess: On success" + uri.getPath());
+                                                //serverFileUri = uri.getPath();
+                                                iv_couverture_livre_bd.setImageURI(localFileUri);
+                                            }
+                                        });
+                            } else {
+                                Log.i(TAG, "onComplete: erreur dans le putFile ");
+                            }
                         }
-                    }
-                });
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i(TAG, "uploadImage: e.getMessage() : " + e.getMessage());
+            Toast.makeText(this, "Erreur lors du chargement de l'image : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
 
 
         Log.i(TAG, "onActivityResult: after upload");
@@ -212,21 +225,18 @@ public class ModifierLivreBD extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Ajout des variables globales des uri cf 7.5
                 localFileUri = data.getData();
+
                 uploadImage();
-                Log.i(TAG, "onActivityResult: "+localFileUri);
+                Log.i(TAG, "onActivityResult: " + localFileUri);
                 // Affectation de l'image sélectionnée à l'avatar (pour la variable globale cf 7.5)
-                iv_couverture_livre.setImageURI(localFileUri);
+
             }
         }
 
 
-
-
-
-        
     }
 
-    public void getListBooksDB(String id_BD){
+    public void getListBooksDB(String id_BD) {
         livresRef.whereEqualTo(documentId(), id_BD).get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -239,7 +249,7 @@ public class ModifierLivreBD extends AppCompatActivity {
                                 title_livre = document.getString(TITRE_LIVRE_BD);
                                 auteur_livre = document.getString(AUTEUR_LIVRE_BD);
                                 couvertureImage = document.getString(URL_COVER_LIVRE_BD);
-                                editeur_livre =document.getString(EDITEUR_LIVRE_BD);
+                                editeur_livre = document.getString(EDITEUR_LIVRE_BD);
                                 parution_livre = document.getString(DATE_PARUTION_LIVRE_BD);
                                 resume_livre = document.getString(RESUME_LIVRE_BD);
                                 isbn_livre = document.getString(ISBN_LIVRE_BD);
@@ -249,7 +259,7 @@ public class ModifierLivreBD extends AppCompatActivity {
 
 
                                 Log.i(TAG, "onComplete: titre : " + title_livre);
-                              //  Log.i(TAG, "nbr_pages : " + nbr_pages);
+                                //  Log.i(TAG, "nbr_pages : " + nbr_pages);
 
                                 tv_title_livre.setText(title_livre);
                                 tv_auteur_livre.setText(auteur_livre);
@@ -262,7 +272,7 @@ public class ModifierLivreBD extends AppCompatActivity {
 
 
                                 //Gestion de l'image avec Glide
-                                Context context = ModifierLivreBD.this ;
+                                Context context = ModifierLivreBD.this;
 
                                 RequestOptions options = new RequestOptions()
                                         .centerCrop()
@@ -275,7 +285,7 @@ public class ModifierLivreBD extends AppCompatActivity {
                                         .apply(options)
                                         .fitCenter()
                                         .diskCacheStrategy(DiskCacheStrategy.ALL)
-                                        .into(iv_couverture_livre);
+                                        .into(iv_couverture_livre_bd);
                             }
                         } else {
                             Log.i(TAG, "Error getting documents: ", task.getException());
@@ -283,8 +293,6 @@ public class ModifierLivreBD extends AppCompatActivity {
                     }
                 });
     }
-
-
 
 
 }
